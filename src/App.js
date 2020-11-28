2c07c18733aeb5766dc0b101a8dc4d2dc3274e42
Melanie W <macbookair@Melanie-W.local>
2020-11-27 22:59:17 -0700

    add pictures

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./tuan512.png" className="App-logo" alt="logo" />
         <p>
           HELLO MEL LETS DO THIS!!!
         </p>
